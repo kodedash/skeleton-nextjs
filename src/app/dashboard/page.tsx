09fc6bfbd86d3da088847ff23ae8f23573ac2cc1
Daniel Henrique <daniel@danielsantos.me>
2025-05-17 23:02:13 -0300

    feat: add initial application configuration, login and dashboard pages, and integrate axios for API calls

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+       <h2 className="text-2xl/7 sm:text-3xl sm:tracking-tight font-bold">Dashboard</h2>
+    </div>
+  );
+}
